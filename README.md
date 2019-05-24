081b42c37d12d5ad736c9b25afa323950b8802d1
Thomas Smith <tsmith123@users.noreply.github.com>
2019-05-24 12:11:34 +0100

    Add Webpack 4

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # React-since
-A React "time-ago" component for those needing "humanistic" outputs rather than "to-the-second" accuracy. For example, instead of **item was added 37 seconds ago** this component would render **item was added just now**. See **Outputs** section below for further information. This package is very small and comes without any of the bloat that you may find in similar packages.
+A tiny component for those needing "humanistic" outputs rather than "to-the-second" accuracy to display the amount of time passed. For example, instead of **item was added 37 seconds ago** this component would render **item was added just now**. See **Outputs** section below for further information.
 
 ## Install
 
@@ -11,12 +11,12 @@ A React "time-ago" component for those needing "humanistic" outputs rather than 
 
 `react-since` requires a `date` prop and returns a `<span></span>` containing the approximate time since that date.
 
-    <Since date="Jan 10, 2017" />
+    <Since date={'2019-05-24 10:30:00.000Z'} />
 
 ## Live Prop
 The component by default is live meaning that dates will auto-update every sixty seconds. You can override this feature by passing a `live` prop like this:
 
-    <Since date="Jan 10, 2017" live={false} />
+    <Since date={'2019-05-24 10:30:00.000Z'} live={false} />
 
 ## Outputs
 `react-since` outputs are designed to be human-friendly. The thinking is that users typically want to know a rough idea of how long ago an event took place and aren't concerned with highly accurate timings. See the table below for example outputs.
---
+++ src/index.js
@@ -0,0 +1,132 @@
+/*
+ * React Since (MIT)
+ * Tom Smith (https://github.com/tsmith123)
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Since extends React.Component {
+  state = {
+    timeoutId: null,
+    since: null
+  }
+
+  tick = () => {
+    const { seconds } = this.props
+
+    const timeoutId = setTimeout(this.tick, 1000 * seconds)
+
+    this.setState({ timeoutId })
+  }
+
+  componentDidMount () {
+    const { live, date } = this.props
+
+    // don't go any further if no date
+    if (!date) return
+
+    this.init(date)
+
+    if (live) {
+      this.tick()
+    }
+  }
+
+  componentWillUnmount () {
+    const { timeoutId } = this.state
+
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+  }
+
+  init = date => {
+    // convert date to milliseconds
+    const then = Date.parse(date)
+    const now = Date.now()
+
+    // get difference in seconds between two dates
+    const seconds = Math.round(Math.abs(now - then) / 1000)
+    const since = this.getSince(seconds)
+
+    this.setState({ since })
+  }
+
+  // the logic - needs cleaning up
+  getSince (seconds) {
+    const minutes = Math.round(seconds / 60)
+    if (minutes < 5) {
+      return 'just now'
+    }
+
+    // round to nearest 5 minutes
+    if (minutes < 45) {
+      return (Math.round(minutes / 5) * 5) + ' minutes ago'
+    }
+
+    if (minutes < 90) {
+      return 'an hour ago'
+    }
+
+    const hours = Math.round(minutes / 60)
+    if (hours < 5) {
+      return 'a few hours ago'
+    }
+
+    if (hours < 12) {
+      return hours + ' hours ago'
+    }
+
+    const days = Math.round(hours / 24)
+    if (days === 1) {
+      return 'yesterday'
+    }
+
+    if (days < 7) {
+      return days + ' days ago'
+    }
+
+    const weeks = Math.round(days / 7)
+    if (weeks === 1) {
+      return 'a week ago'
+    }
+    if (weeks < 4) {
+      return weeks + ' weeks ago'
+    }
+
+    const months = Math.round(weeks / 4)
+    if (months === 1) {
+      return 'a month ago'
+    }
+    if (months < 12) {
+      return months + ' months ago'
+    }
+
+    const years = Math.round(months / 12)
+    if (years === 1) {
+      return 'a year ago'
+    }
+
+    return years + ' years ago'
+  }
+
+  render () {
+    const { since } = this.state
+
+    return <span>{since}</span>
+  }
+}
+
+Since.propTypes = {
+  live: PropTypes.bool,
+  date: PropTypes.string,
+  seconds: PropTypes.number
+}
+
+Since.defaultProps = {
+  live: true,
+  seconds: 60
+}
+
+export default Since
